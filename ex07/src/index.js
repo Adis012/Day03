175a7d3747a9ac2623eaf3a40701658493bd3a75
Adis012 <adis012@hotmail.com>
2021-03-10 21:38:42 +0100

    adding index

---
+++ ex07/src/index.js
@@ -0,0 +1,30 @@
+var drawImage;
+var newImageName;
+var thisImageNameIsSoLong;
+
+/FOOBOO/
+var studlycapvar;
+var propercapvar;
+var titlecaseover;
+
+/ FooBoo/
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLECaseOver = 9000;
+
+function main() {
+    / fooboo/
+    / fooboo declarations/
+
+    /fooboo assignments/
+
+    /fooboo/
+    return {
+        studlycapvar,
+        PRoperCAmelCAse,
+        titlecaseover
+    }
+}
+
+console.log(main));
+module.exports = main;
