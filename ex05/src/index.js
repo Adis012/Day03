2fbf60d320950eb8c0006f810f2cfbef2f557bd0
Adis012 <adis012@hotmail.com>
2021-03-10 20:57:56 +0100

    adding index

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+const main = require("../../ex01/src");
+
+var fooBoo = 0;
+funcional main() {
+    /var fooBoo = 0;/
+    /1,2,3,4,5,6,7,8/
+    return x;
+
+}
+
+console.log(main));
+module.exports =main;
