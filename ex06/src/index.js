8f7b1b4bb473e6440fe87d17993f41a664282493
Adis012 <adis012@hotmail.com>
2021-03-10 21:19:41 +0100

    adding index

---
+++ ex06/src/index.js
@@ -0,0 +1,20 @@
+/undefinded/
+var x 7;
+var y 12;
+var z "My name is";
+/programiranje/
+
+x = 7 + 2;
+y = 12 + 4;
+z = "My name is" + "John";
+function main() {
+    /undefinded/
+
+    x = 7 + 2;
+    y = 12 + 4;
+    z = "My name is" + "John"; 
+    return [x, y, z];
+}
+
+console.log(main));
+module.exports =main;
